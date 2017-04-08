3cea6d281b4f21c2b82e1015c10553b5a5bed810
Satyam Kumar <satyamk618@gmail.com>
2017-04-08 17:35:24 +0530

    Add files via upload

---
+++ p2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<pthread.h>
+void * add(void *a)
+{
+	int *b;
+	b=(int *) a;
+	int sum;
+	sum= b[0] + b[1];
+	return (void *)sum;
+}
+int main()
+{
+	int c;
+	pthread_t tid;
+	pthread_attr_t attr;
+	
+	pthread_attr_init (&attr);
+        pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_DETACHED);
+	int a[2]={3,5};
+	pthread_create(&tid,NULL,&add,a);
+	pthread_attr_destroy(&attr);
+	pthread_join(tid,(void *)&c);
+	printf("\n%d\n", c);
+	return 0;
+}
